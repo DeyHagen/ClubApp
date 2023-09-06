30d4c519b52119dbebd50a89a9dccc4e7d635c55
David Gakhokidze <dgakhokidze@outlook.com>
2023-09-06 23:15:24 +0400

    IView Interface

---
+++ View/IView.cs
@@ -0,0 +1,15 @@
+﻿using ClassMembershipApplication.FieldValidators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMembershipApplication.View
+{
+    public interface IView
+    {
+        void RunView();
+        IFieldValidator fieldValidator { get; set; }
+    }
+}
