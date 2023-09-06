b12b9c6517737a7109b5d987a655e976a403608f
David Gakhokidze <dgakhokidze@outlook.com>
2023-09-06 23:11:48 +0400

    ComonOutputFormat.cs

--- CommonOutputText.cs
+++ CommonOutputText.cs
@@ -33,5 +33,29 @@ namespace ClassMembershipApplication
                 return $"{heading}{Environment.NewLine}{new string('-', heading.Length)}";
             }
         }
+        public static void WriteMainHeading()
+        {
+            Console.Clear();
+            Console.WriteLine(MainHeading);
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+        public static void WriteLoginHeading()
+        {
+            Console.Clear();
+            Console.WriteLine(LoginHeading);
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+        public static void WriteRegistrationHeading()
+        {
+            Console.Clear();
+            Console.WriteLine(RegistrationHeading);
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
     }
 }
